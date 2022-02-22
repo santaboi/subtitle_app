1e6253ea9d2af130dd42d40435868bdafc0acf90
Jeff <egg20001225@gmail.com>
2022-02-22 22:48:07 +0800

    translator update

--- main/translator.py
+++ main/translator.py
@@ -4,11 +4,11 @@ from googletrans import LANGUAGES as LG
 import os
 src = "en"
 dest = 'zh-tw'
-file_name = "text1.txt"
+file_name = "shook_ones.txt"
 
 translator_dir = os.path.dirname(__file__) #absolute path of this code
 rel_path = "lyrics/" + file_name
-out_rel_path = "output_lyrics/" + file_name
+out_rel_path = "output_lyrics/" + file_name.split('.')[0] + "_new.txt"
 lyrics_path = os.path.join(translator_dir , rel_path)
 out_path = os.path.join(translator_dir , out_rel_path)
 
@@ -19,13 +19,13 @@ try :
     with open(out_rel_path , "w") as fo :
         translator = Translator()
         for text_per_line in lyrics :
-            """translation = translator.translate(text_per_line , dest ="zh-tw")
+            translation = translator.translate(text_per_line , dest ="zh-tw").text
             print(text_per_line)
-            print(translation.text)"""
-            translator = Translator()
-            translation = translator.translate("Der Himmel ist blau und ich mag Bananen", dest="en")
-            print(translation.text)
-            #fo.write(text_per_line)
-            #fo.write(translation.text)
+            print(translation)
+            fo.write(text_per_line)
+            fo.write(translation)
+            fo.write("\n\n")
+        fo.close()
 except FileNotFoundError:
     print("The 'docs' directory does not exist")
+lyrics.close()
